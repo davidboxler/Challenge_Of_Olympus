8a8a4e4b32b179b03cf1a93c68cf538417176210
Agustin <agustintour.3@gmail.com>
2024-09-09 21:03:46 -0300

    ajuste de texto minimo

--- src/scenes/ModeSel.js
+++ src/scenes/ModeSel.js
@@ -33,5 +33,16 @@ export class ModeSel extends Scene
             this.scene.start('Versus');
 
         });
+        const backText = this.add.text(750, 650, 'Volver', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5).setInteractive();
+
+        backText.on('pointerdown', () => {
+
+            this.scene.start('MainMenu');
+
+        });
       }
 }
