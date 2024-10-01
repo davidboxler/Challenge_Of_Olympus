a5cd10f9517da89fdda946a64c4972efe8511397
Agustin <agustintour.3@gmail.com>
2024-10-01 16:45:02 -0300

    ajustes rapidos

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -12,9 +12,8 @@ export class MainMenu extends Scene
         this.add.image(1250, 1000, 'MainMenuTheme');
 
 
-        const gameText = this.add.text(1250, 855, 'Play', {
-            fontFamily: 'Arial Black', fontSize: 78, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+        const gameText = this.add.text(1250, 855, 'Jugar', {
+            fontFamily: 'Arial Black', fontSize: 70, color: '#ffffff',
             align: 'center'
         }).setOrigin(0.5).setInteractive();
 
@@ -23,9 +22,8 @@ export class MainMenu extends Scene
             this.scene.start('GameCapas');
 
         });
-        const optText = this.add.text(1250, 1000, 'Options', {
-            fontFamily: 'Arial Black', fontSize: 78, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+        const optText = this.add.text(1250, 1000, 'Opciones', {
+            fontFamily: 'Arial Black', fontSize: 70, color: '#ffffff',
             align: 'center'
         }).setOrigin(0.5).setInteractive();
 
